3b4c97010411f1011ca0233baaf1a9868a444a86
Raymond Camden <raymondcamden@gmail.com>
2014-03-02 10:31:08 -0600

    first

---
+++ barcode/www/js/app.js
@@ -0,0 +1,23 @@
+var resultDiv;
+
+document.addEventListener("deviceready", init, false);
+function init() {
+	document.querySelector("#startScan").addEventListener("touchend", startScan, false);
+	resultDiv = document.querySelector("#results");
+}
+
+function startScan() {
+
+	cordova.plugins.barcodeScanner.scan(
+		function (result) {
+			var s = "Result: " + result.text + "<br/>" +
+			"Format: " + result.format + "<br/>" +
+			"Cancelled: " + result.cancelled;
+			resultDiv.innerHTML = s;
+		}, 
+		function (error) {
+			alert("Scanning failed: " + error);
+		}
+	);
+
+}
