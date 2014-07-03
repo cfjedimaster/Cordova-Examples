46fac24f4f697de965a2a358a6103a458146cebb
Raymond Camden <raymondcamden@gmail.com>
2014-07-02 22:43:01 -0500

    simplified version, thx to Andrew Grieve

--- checkanddownload/www/js/app.js
+++ checkanddownload/www/js/app.js
@@ -1,7 +1,7 @@
 document.addEventListener("deviceready", init, false);
 
-//Will be a directory entry object for our data store
-var storeOb;
+//The directory to store data
+var store;
 
 //Used for status updates
 var $status;
@@ -10,7 +10,7 @@ var $status;
 var assetURL = "https://raw.githubusercontent.com/cfjedimaster/Cordova-Examples/master/readme.md";
 
 //File name of our important data file we didn't ship with the app
-var fileName = "data.txt";
+var fileName = "mydatafile.txt";
 
 function init() {
 	
@@ -18,33 +18,17 @@ function init() {
 
 	$status.innerHTML = "Checking for data file.";
 
-	//The directory to store data
-	var store = cordova.file.dataDirectory;
+	store = cordova.file.dataDirectory;
 
-	//The Object version - no error handler because this must work
-	window.resolveLocalFileSystemURL(store, function(ob) {
-
-		storeOb = ob;
-
-		//Let's check the directory to see if our file exists
-		storeOb.getFile(fileName, {create:false}, function(f) {
-			console.log('yes, the file existed');
-			appStart();
-		}, function(e) {
-			console.log('file did not exist');
-			$status.innerHTML = "We need to download the file.";
-			downloadAsset();
-		});
-
-
-	});
+	//Check for the file. 
+	window.resolveLocalFileSystemURL(store + fileName, appStart, downloadAsset);
 
 }
 
 function downloadAsset() {
 	var fileTransfer = new FileTransfer();
 	console.log("About to start transfer");
-	fileTransfer.download(assetURL, storeOb.toURL() + fileName, 
+	fileTransfer.download(assetURL, store + fileName, 
 		function(entry) {
 			console.log("Success!");
 			appStart();
