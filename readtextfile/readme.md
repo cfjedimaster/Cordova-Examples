f8d9111e749a0c8343c3ea64ea16799dc25cfb77
Raymond Camden <raymondcamden@gmail.com>
2014-07-15 13:46:28 -0500

    read text file example

---
+++ readtextfile/readme.md
@@ -0,0 +1,10 @@
+Read a text file Example
+===
+
+This example demonstrates how to read a text file. To run, add the FileSystem
+plugin:
+
+    cordova plugin add org.apache.cordova.file
+
+When run, the code will read the contents of index.html and place it
+in the textarea.
---
+++ readtextfile/www/js/app.js
@@ -0,0 +1,33 @@
+document.addEventListener("deviceready", init, false);
+function init() {
+	
+	//This alias is a read-only pointer to the app itself
+	window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "www/index.html", gotFile, fail);
+
+	/* Yes, this works too for our specific example...
+	$.get("index.html", function(res) {
+		console.log("index.html", res);
+	});
+	*/
+
+}
+
+function fail(e) {
+	console.log("FileSystem Error");
+	console.dir(e);
+}
+
+function gotFile(fileEntry) {
+
+	fileEntry.file(function(file) {
+		var reader = new FileReader();
+
+		reader.onloadend = function(e) {
+			console.log("Text is: "+this.result);
+			document.querySelector("#textArea").innerHTML = this.result;
+		}
+
+		reader.readAsText(file);
+	});
+
+}
