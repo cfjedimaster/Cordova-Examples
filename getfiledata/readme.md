71a8a53db6b37ed96cea9fb4057facfcb9f0d626
Raymond Camden <raymondcamden@gmail.com>
2014-08-18 16:57:25 -0500

    example of file metadata

---
+++ getfiledata/readme.md
@@ -0,0 +1,12 @@
+Fil Metadata Example
+===
+
+This application requests a file so that it can fetch metadata about
+the file.
+
+Please add the following plugin:
+
+	cordova plugin add org.apache.cordova.file
+
+When the application launches, it will fetch www/index.html and display
+metadata about it in the DOM. It also dumps the object to the console.
---
+++ getfiledata/www/js/app.js
@@ -0,0 +1,28 @@
+document.addEventListener("deviceready", init, false);
+function init() {
+	
+	//This alias is a read-only pointer to the app itself
+	window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "www/index.html", gotFile, fail);
+
+
+}
+
+function fail(e) {
+	console.log("FileSystem Error3");
+	console.dir(e);
+}
+
+function gotFile(fileEntry) {
+
+	fileEntry.file(function(file) {
+		var s = "";
+		s += "<b>name:</b> " + file.name + "<br/>";
+		s += "<b>localURL:</b> " + file.localURL + "<br/>";
+		s += "<b>type:</b> " + file.type + "<br/>";
+		s += "<b>lastModifiedDate:</b> " + (new Date(file.lastModifiedDate)) + "<br/>";
+		s += "<b>size:</b> " + file.size + "<br/>";
+		
+		document.querySelector("#status").innerHTML = s;
+		console.dir(file);
+	});
+}
