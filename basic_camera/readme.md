805490b54b11edcb8300e59afdb5602ea6e38a56
Raymond Camden <raymondcamden@gmail.com>
2014-09-23 09:12:08 -0500

    basic camera demo

---
+++ basic_camera/readme.md
@@ -0,0 +1,6 @@
+Basic Camera Example
+===
+
+A super simple camera demo. Allows you to take from the camera or library.
+
+    cordova plugin add org.apache.cordova.camera
---
+++ basic_camera/www/js/app.js
@@ -0,0 +1,46 @@
+document.addEventListener("deviceready", init, false);
+function init() {
+
+	function onSuccess(imageData) {
+		console.log('success');
+		var image = document.getElementById('myImage');
+		image.src = imageData;
+		console.log(imageData);
+	}
+
+	function onFail(message) {
+		alert('Failed because: ' + message);
+	}	
+
+	//Use from Camera
+	document.querySelector("#takePicture").addEventListener("touchend", function() {
+		navigator.camera.getPicture(onSuccess, onFail, { 
+			quality: 50,
+			sourceType: Camera.PictureSourceType.CAMERA,
+			destinationType: Camera.DestinationType.FILE_URI
+		});
+
+		function onSuccess(imageData) {
+			console.log('success');
+			var image = document.getElementById('myImage');
+			image.src = imageData;
+			console.log(imageData);
+		}
+
+		function onFail(message) {
+			alert('Failed because: ' + message);
+		}
+
+	});
+
+	//Use from Library
+	document.querySelector("#usePicture").addEventListener("touchend", function() {
+		navigator.camera.getPicture(onSuccess, onFail, { 
+			quality: 50,
+			sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+			destinationType: Camera.DestinationType.FILE_URI
+		});
+
+	});
+
+}
