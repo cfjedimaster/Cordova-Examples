e904863a0b4cffca407ba9aaf368a794e1be4603
Raymond Camden <raymondcamden@gmail.com>
2016-07-07 11:11:24 -0500

    final version

--- ionicnative_shake/app/pages/home/home.ts
+++ ionicnative_shake/app/pages/home/home.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController,Platform} from 'ionic-angular';
 import {CatProvider} from '../../providers/cat-provider/cat-provider';
+import {DeviceMotion} from 'ionic-native';
 
 @Component({
   providers: [CatProvider],
@@ -9,9 +10,49 @@ import {CatProvider} from '../../providers/cat-provider/cat-provider';
 export class HomePage {
 
   public cats:Array<Object>;
+  private lastX:number;
+  private lastY:number;
+  private lastZ:number;
+  private moveCounter:number = 0;
 
-  constructor(public catProvider:CatProvider, private navController: NavController) {
+  constructor(public catProvider:CatProvider, private navController: NavController, platform:Platform) {
     this.loadCats();
+
+    platform.ready().then(() => {
+      var subscription = DeviceMotion.watchAcceleration({frequency:200}).subscribe(acc => {
+        //console.log(acc);
+
+        if(!this.lastX) {
+          this.lastX = acc.x;
+          this.lastY = acc.y;
+          this.lastZ = acc.z;
+          return;
+        }
+
+        let deltaX:number, deltaY:number, deltaZ:number;
+        deltaX = Math.abs(acc.x-this.lastX);
+        deltaY = Math.abs(acc.y-this.lastY);
+        deltaZ = Math.abs(acc.z-this.lastZ);
+
+        if(deltaX + deltaY + deltaZ > 3) {
+          this.moveCounter++;
+        } else {
+          this.moveCounter = Math.max(0, --this.moveCounter);
+        }
+
+        if(this.moveCounter > 2) { 
+          console.log('SHAKE');
+          this.loadCats(); 
+          this.moveCounter=0; 
+        }
+
+        this.lastX = acc.x;
+        this.lastY = acc.y;
+        this.lastZ = acc.z;
+
+      });
+    });
+
   }
 
   loadMore() {
@@ -21,7 +62,6 @@ export class HomePage {
 
   loadCats() {
     this.catProvider.load().then(result => {
-      console.log('eh?');
       this.cats = result;
     });
   }
