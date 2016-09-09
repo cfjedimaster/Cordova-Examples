d4e044de7a1f0fb63f8e03495f3c3e19ddbc4770
Raymond Camden <raymondcamden@gmail.com>
2016-09-09 08:32:17 -0500

    fixes

--- loopingaudio/www/js/index.js
+++ loopingaudio/www/js/index.js
@@ -3,8 +3,13 @@ var media;
 
 function init() {
 	console.log('init');
-	media = new Media('arcade1.mp3', mediaSuccess, mediaError, mediaStatus);
+	media = new Media(getMediaURL('arcade1.mp3'), null, mediaError, mediaStatus);
 	media.play();
+}
+
+function getMediaURL(s) {
+    if(device.platform.toLowerCase() === "android") return "/android_asset/www/" + s;
+    return s;
 }
 
 function mediaError(e) {
@@ -12,7 +17,7 @@ function mediaError(e) {
 }
 
 function mediaStatus(status) {
-	if(status === 4) {
+	if(status === Media.MEDIA_STOPPED) {
 		media.seekTo(0);
 		media.play();
 	}
