2cc490af75357e569ec3080b0482d3accbc5e4fc
Raymond Camden <raymondcamden@gmail.com>
2017-01-19 11:59:53 -0600

    db3

---
+++ db3/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
+
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { Auth, Database } from '@ionic/cloud-angular';
+
+
+@Component({
+  template: `<ion-nav [root]="rootPage"></ion-nav>`
+})
+export class MyApp {
+  rootPage;
+
+  constructor(platform: Platform, public auth:Auth, public db:Database) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+
+      if(this.auth.isAuthenticated()) {
+        this.db.connect();
+        this.rootPage = HomePage;
+      } else {
+        this.rootPage = LoginPage;
+      }
+      
+    });
+  }
+}
