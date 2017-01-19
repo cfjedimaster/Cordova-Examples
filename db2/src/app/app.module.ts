d94db705a5647c636d17919037810c4a40085771
Raymond Camden <ray@DeathStar.(none)>
2017-01-19 13:47:01 -0600

    upd

--- db2/src/app/app.module.ts
+++ db2/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 import { HomePage } from '../pages/home/home';
@@ -30,6 +30,6 @@ const cloudSettings: CloudSettings = {
     HomePage,
     LoginPage
   ],
-  providers: []
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {}
